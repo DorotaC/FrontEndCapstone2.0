eeb48c58d2d5747df6614a2a92a32f37478680cf
DorotaC <dorota.delezynska@gmail.com>
2021-04-13 16:50:46 +0200

    First part of cleaning - app.js (code cleaned, comments added)

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -2,69 +2,61 @@
 let d = new Date();
 let newDate = (d.getMonth()+1)+'/'+ d.getDate()+'/'+ d.getFullYear();
 
+//Function used to generate event listener after DOM is loaded (check index.js)
 function init() {
   const generateButton = document.getElementById('generate');
-
   generateButton.addEventListener('click', generateAndPost);
 }
+
 //Main function that gets data from API as well as user data and dynamically update UI
 function generateAndPost(event) {
   const cityField = document.getElementById('city');
   const tripDateRaw = document.getElementById('tripDate');
   const tripDate = new Date(tripDateRaw.value);
-  //console.log('pattern mismatch: ' + tripDateRaw.validity.patternMismatch)
+
+  //checking if city name is valid. If it is, then date format is checked. If everything is ok, app proceeds.
   if (cityField.validity.patternMismatch || cityField.value == ''){
     alert('City name is not correct. Only latin letters and dash symbol are accepted \'\-\'.');
-    //line below produces error because of Uncaught TypeError: Cannot read property 'patternMismatch' of undefined at HTMLButtonElement.i
     } else if (tripDateRaw == '' || (tripDate < d) || tripDateRaw.validity.patternMismatch) {
-    //} else if (tripDateRaw == '' || (tripDate < d)) {
       alert('Date is not correct. Correct format is MM/DD/YYYY. You cannot plan departure earlier than tommorow')
     } else {
-    const city = cityField.value;
-    const tripDateRefined = (tripDate.getMonth()+1)+'/'+ tripDate.getDate()+'/'+ tripDate.getFullYear();
-    const daysToTrip = Math.ceil((tripDate.getTime() - d.getTime())/(1000 * 3600 * 24));
-    Client.postHandler('/getLocation', {uInput: city})
-     .then(function(cityData){
-       console.log("cityData: ")
-       console.log(cityData)
-       if ('message' in cityData){
-         alert(cityData.message)
-       } else {
-         //Client.postHandler('/weather', {cityName: cityData.main.name, date: newDate, response: response});
-         // Client.postHandler('/cityData', {cityName: cityData.geonames[0].name,
-         //                                 country: cityData.geonames[0].countryName,
-                                         // latitude: cityData.geonames[0].lat,
-                                         // longitude: cityData.geonames[0].lng,
-                                         // tripDate: tripDateRefined,
-                                         // daysToTrip: daysToTrip});
-         Client.postHandler('/cityData', {cityName: cityData.cityName,
-                                         country: cityData.country,
-                                         lowTempCurrent: cityData.lowTempCurrent,
-                                         lowTempForecast: cityData.lowTempForecast,
-                                         maxTempCurrent: cityData.maxTempCurrent,
-                                         maxTempForecast: cityData.maxTempForecast,
-                                         cloudsCurrent: cityData.cloudsCurrent,
-                                         cloudsForecast: cityData.cloudsForecast,
-                                         tripDate: tripDateRefined,
-                                         daysToTrip: daysToTrip,
-                                         photo: cityData.photo});
-      }
-      })
-     .then(function() {
-       updateUI()
-      });
-       }
-
-  }
-//}
+        const city = cityField.value;
+        //Number of days to wait for the trip is calculated, based on value from user input
+        const tripDateRefined = (tripDate.getMonth()+1)+'/'+ tripDate.getDate()+'/'+ tripDate.getFullYear();
+        const daysToTrip = Math.ceil((tripDate.getTime() - d.getTime())/(1000 * 3600 * 24));
+        //city name from user input is send to server side to get through all the APIs. As a result, app data is received.
+        Client.postHandler('/getLocation', {uInput: city})
+         .then(function(cityData){
+           //in case no city/place was found in Geonames database, an alert will appear.
+           if ('message' in cityData){
+             alert(cityData.message)
+           } else {
+             //complete city data are being send to appear on the website as a 'most recent entry'
+             Client.postHandler('/cityData', {cityName: cityData.cityName,
+                                             country: cityData.country,
+                                             lowTempCurrent: cityData.lowTempCurrent,
+                                             lowTempForecast: cityData.lowTempForecast,
+                                             maxTempCurrent: cityData.maxTempCurrent,
+                                             maxTempForecast: cityData.maxTempForecast,
+                                             cloudsCurrent: cityData.cloudsCurrent,
+                                             cloudsForecast: cityData.cloudsForecast,
+                                             tripDate: tripDateRefined,
+                                             daysToTrip: daysToTrip,
+                                             photo: cityData.photo});
+          }
+        })
+        //function that make city/trip data appear on the screen
+        .then(function() {
+          updateUI()
+          });
+    }
+}
 
 //function that dynamically update UI
 const updateUI = async () => {
   const req = await fetch('/all');
   try {
     const reqData = await req.json();
-    console.log('reqData')
-    console.log(reqData);
     document.getElementById('cityName').innerHTML = reqData.cityName;
     document.getElementById('country').innerHTML = reqData.country;
     document.getElementById('date').innerHTML = reqData.tripDate;
@@ -79,7 +71,6 @@ const updateUI = async () => {
         document.getElementById('clouds').innerHTML = reqData.cloudsForecast;
     }
     document.getElementById("photo").src = reqData.photo;
-    console.log(photo)
   } catch(error) {
     console.log(error);
   }
